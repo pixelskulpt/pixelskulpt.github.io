cc1294db69bf35358de291784cc06024ac45aa1a
circusagency <circus.agency2020@gmail.com>
2020-11-20 22:13:16 +0100

    animation fadein

---
+++ assets/js/fadeIn.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $("#carouselDIV").addClass("opacity1")
+  setTimeout(function(){
+    $("#logoDIV, #titleDIV, #menuDIV, #socialDIV, #privacyDIV, #bulletDIV, #navDIV")
+      .addClass("opacity1")
+  }, 1500)
+  setTimeout(function(){
+    $("#callDIV").addClass("opacity1");
+  }, 2400);
+});
