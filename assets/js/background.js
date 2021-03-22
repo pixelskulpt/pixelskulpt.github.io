26b48dc4b8efe49b6c36167ae80fc37ae8718a11
circusagency <circus.agency2020@gmail.com>
2021-03-22 10:18:15 +0100

    last

--- assets/js/background.js
+++ assets/js/background.js
@@ -1,3 +1,8 @@
-function changeBodyBg(color){
-    document.body.style.background = color;
-}
+//$(document).ready(function () {
+
+//var back = ["#fb3449","#ff5b35","#cc6d4e","#4ea685","#46797b","#375d9d","#683277"];
+//$(".colorB").click(function(){
+//  var rand = back[Math.floor(Math.random() * back.length)];
+//    $('body').css('background',rand);
+//});
+//});
--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,11 +1,8 @@
 $(document).ready(function() {
   $('#contactB').click(function() {
-    $('.foto').css("opacity", "1");
-  });
-  $('#workB').click(function() {
     $('.foto').css("opacity", "0");
   });
-    $('#aboutB').click(function() {
-      $('.foto').css("opacity", "0");
+  $('.colorB').click(function() {
+    $('.foto').css("opacity", "0");
   });
 });
